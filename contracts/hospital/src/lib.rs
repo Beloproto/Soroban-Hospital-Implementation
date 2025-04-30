75e20a7f6e97d07c59f4189f6004ece1c321ce1e
Adebara123 <adebaraayomide@gmail.com>
2025-04-30 19:07:04 +0100

    Chore: added some patient functionality

--- contracts/hospital/src/lib.rs
+++ contracts/hospital/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, contracttype, symbol_short, vec, Env, String, Vec};
+use soroban_sdk::{contract, contractimpl, contracttype, symbol_short, vec, Address, Env, String, Vec};
 
 
 // Hospital contract Outline 
@@ -29,7 +29,7 @@ use soroban_sdk::{contract, contractimpl, contracttype, symbol_short, vec, Env, 
 // -> list of medical tests 
 
 // Things to take note 
-// Data is stored 
+// Structure of data  
 // How to retreive data and store data 
 // How to get environment variables 
 
@@ -68,16 +68,20 @@ pub struct MedicalTest {
     results: String,
     notes: String,
 }
-
-
- // pateient : id -> name -> PatientData
- // id -> PatientData 
+ 
 
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum DataKey {
     Admin,
-    Patient(u64)
+    Patient(u64),
+    Doctor(u64),
+    MedicalTest(u64),
+    PatientTests(u64),
+    DoctorTests(u64),
+    PatientCount,
+    DoctorCount,
+    TestCount,
 }
 
 
@@ -86,9 +90,111 @@ pub struct HospitalContract;
 
 #[contractimpl]
 impl HospitalContract {
-   fn initialize() {
-    todo!()
+
+    // Initializer -> It initialize the contract with the admin 
+   pub fn initialize(env: Env, admin: Address) -> Address {
+
+    if env.storage().instance().has(&DataKey::Admin) {
+        panic!("Contract already initilized");
+    }
+
+    env.storage().instance().set(&DataKey::Admin, &admin);
+    env.storage().instance().set(&DataKey::PatientCount, &0u64);
+    env.storage().instance().set(&DataKey::DoctorCount, &0u64);
+    env.storage().instance().set(&DataKey::TestCount, &0u64);
+
+    admin
    }
+
+   fn check_admin (env: &Env) {
+    let admin: Address = env.storage().instance().get(&DataKey::Admin).unwrap();
+    admin.require_auth();
+   }
+
+   // Patient Managment Functions 
+
+   pub fn register_patient (
+    env: Env,
+    name: String,
+    date_of_birth: u64,
+    blood_type: String,
+    allergies: Vec<String>,
+    insurance_id: String,
+   ) -> u64 {
+        Self::check_admin(&env);
+
+        // Get and increment patient count
+        let patient_count: u64 = env.storage().instance().get(&DataKey::PatientCount).unwrap_or(0);
+        let new_id = patient_count + 1;
+
+        // create the patiet record 
+        let patient = Patient {
+            id: new_id,
+            name,
+            date_of_birth,
+            blood_type,
+            allergies,
+            insurance_id,
+            active: true,
+        };
+        
+        // Store the patient data and update update the count 
+
+        env.storage().instance().set(&DataKey::Patient(new_id), &patient);
+        env.storage().instance().set(&DataKey::PatientCount, &new_id);
+
+        // Initialize empty test list for patient 
+
+        env.storage().instance().set(&DataKey::PatientTests(new_id), &Vec::<u64>::new(&env));
+
+        new_id
+   }
+
+   // Get patient information 
+
+   pub fn get_patient(env: Env, id: u64) -> Patient {
+        match env.storage().instance().get(&DataKey::Patient(id)) {
+            Some(patient) => patient,
+            None => panic!("Patient not registered"),
+        }
+   }
+
+
+   // Update patient record 
+   pub fn update_patient (
+    env: Env,
+    id: u64,
+    name: String,
+    date_of_birth: u64,
+    blood_type: String,
+    allergies: Vec<String>,
+    insurance_id: String
+   ) -> Patient {
+    Self::check_admin(&env);
+
+    // Get existing patient 
+    let mut patient: Patient = env.storage().instance().get(&DataKey::Patient(id)).
+        unwrap_or_else(|| panic!("Patient not found"));
+
+
+    // Update fields 
+    patient.name = name;
+    patient.date_of_birth = date_of_birth;
+    patient.blood_type = blood_type;
+    patient.allergies = allergies;
+    patient.insurance_id = insurance_id;
+
+    // Save the updated patient 
+    env.storage().instance().set(&DataKey::Patient(id), &patient);
+
+    patient
+    
+   }
+
+
+
+
+
 }
 
 mod test;
